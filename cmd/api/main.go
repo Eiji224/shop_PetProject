e478bb2d2c845e4d4487902b142eb00cb050625f
Eiji224 <andremaps06@gmail.com>
2025-12-12 01:04:27 +0500

    Dockerfile and docker-compose.yml

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	_ "shop/docs"
 	"shop/internal/app"
 	"shop/internal/env"
 	"shop/internal/repositories"
+	"time"
 
 	_ "github.com/joho/godotenv/autoload"
 	"gorm.io/driver/mysql"
@@ -42,10 +44,26 @@ func connectDB() *gorm.DB {
 		env.GetEnvString("DB_PORT", "3306"),
 		env.GetEnvString("DB_NAME", "mysql"),
 	)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
+
+	var db *gorm.DB
+	var err error
+	maxRetries := 10
+	retryDelay := 3 * time.Second
+
+	for i := 0; i < maxRetries; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			log.Println("Успешно подключено к базе данных")
+			return db
+		}
+
+		log.Printf("Попытка подключения к БД %d/%d не удалась: %v", i+1, maxRetries, err)
+		if i < maxRetries-1 {
+			log.Printf("Повторная попытка через %v...", retryDelay)
+			time.Sleep(retryDelay)
+		}
 	}
 
-	return db
+	log.Printf("Не удалось подключиться к БД после %d попыток. DSN: %s", maxRetries, dsn)
+	panic(fmt.Sprintf("не удалось подключиться к базе данных: %v", err))
 }
